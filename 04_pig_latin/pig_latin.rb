aa524f2a8f0eb984306a94227a1f0a2dffa36d02
Bliss Man <blissman@gmail.com>
2016-01-18 17:47:19 -0500

    rewrote to detect all consontants before a vowel

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,10 +1,21 @@
 def translate(string)
   array = string.split("")
-  if array[0] == ( "a" || "e" || "i" || "o" || "u" )
-    new_string = string + "ay"
-  else
-      sliced = array.slice!(0)
-      new_string = array.join("") + sliced + "ay"
+  index_num = -1
+    array.each do |character|
+    if character == "a" || character == "e" || character == "i" || character == "o" || character == "u"
+        index_num = array.index(character)
+        break
+    end
   end
-  new_string
+  sliced = array.slice!(0..(index_num - 1))
+  # puts sliced
+  new_string = array.join("") + sliced.join("") + "ay"
+  #
+  # if array[0] == ( "a" || "e" || "i" || "o" || "u" )
+  #   new_string = string + "ay"
+  # else
+  #     sliced = array.slice!(0)
+  #     new_string = array.join("") + sliced + "ay"
+  # end
+  # new_string
 end
