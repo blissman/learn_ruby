16332bcc4b66553fe2bbaf13c62677bafb4929ce
Bliss Man <blissman@gmail.com>
2016-01-18 16:49:19 -0500

    test for apple, banana passed!

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,10 @@
 def translate(string)
   array = string.split("")
-  if array[0] == "a" || "e" || "i" || "o" || "u"
+  if array[0] == ( "a" || "e" || "i" || "o" || "u" )
     new_string = string + "ay"
+  else
+      sliced = array.slice!(0)
+      new_string = array.join("") + sliced + "ay"
   end
   new_string
 end
